dcb0c62111bf515cafd7a66a31361234cb20ee48
Moballigh5225 <moballigh5225@gmail.com>
2023-12-28 18:05:43 +0530

    product page created and added all products

---
+++ frontend/src/products.js
@@ -0,0 +1,55 @@
+const products = [
+  {
+    id: "1",
+    name: "Airpods Wireless Bluetooth Headphones",
+    image: "/images/airpods.jpg",
+    description:
+      "AirPods deliver the wireless headphone experience, reimagined. Just pull them out of the Lightning Charging Case and they're ready to use with your iPhone, Apple Watch, iPad or Mac. After a simple one-tap setup, AirPods work like magic. They're automatically on and always connected.",
+    brand: "Apple",
+    category: "Electronics",
+    price: 89.99,
+    countInStock: 10,
+    rating: 3.9,
+    numReviews: 10,
+  },
+  {
+    id: "2",
+    name: "iphone 11 pro 256gb Memomry",
+    image: "/images/phone.jpg",
+    description:
+      "AirPods deliver the wireless headphone experience, reimagined. Just pull them out of the Lightning Charging Case and they're ready to use with your iPhone, Apple Watch, iPad or Mac. After a simple one-tap setup, AirPods work like magic. They're automatically on and always connected.",
+    brand: "Apple",
+    category: "Electronics",
+    price: 441.99,
+    countInStock: 10,
+    rating: 4.2,
+    numReviews: 11,
+  },
+  {
+    id: "3",
+    name: "Canon EOS 80D DSLR Camera",
+    image: "/images/camera.jpg",
+    description:
+      "AirPods deliver the wireless headphone experience, reimagined. Just pull them out of the Lightning Charging Case and they're ready to use with your iPhone, Apple Watch, iPad or Mac. After a simple one-tap setup, AirPods work like magic. They're automatically on and always connected.",
+    brand: "Apple",
+    category: "Electronics",
+    price: 112.99,
+    countInStock: 10,
+    rating: 4.0,
+    numReviews: 7,
+  },
+  {
+    id: "4",
+    name: "Sony Playstation 4 Pro White Version",
+    image: "/images/playstation.jpg",
+    description:
+      "AirPods deliver the wireless headphone experience, reimagined. Just pull them out of the Lightning Charging Case and they're ready to use with your iPhone, Apple Watch, iPad or Mac. After a simple one-tap setup, AirPods work like magic. They're automatically on and always connected.",
+    brand: "Apple",
+    category: "Electronics",
+    price: 442.99,
+    countInStock: 0,
+    rating: 4.5,
+    numReviews: 13,
+  },
+];
+export default products;
