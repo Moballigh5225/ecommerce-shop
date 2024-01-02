6713cdabc5a4997001f620eb581b9203ea8070f8
Moballigh5225 <moballigh5225@gmail.com>
2024-01-03 02:42:23 +0530

    Mongo DB and backend server setup

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,13 +1,22 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
 import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
 import Rating from "../components/Rating";
-import products from "../products";
 
 const ProductScreen = () => {
+  const [product, setProduct] = useState([]);
+
   const { productId } = useParams();
-  const product = products.find((p) => p.id === productId);
-  console.log(product);
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${productId}`);
+      setProduct(data);
+      console.log(data, "data is hereee");
+    };
+    fetchProduct();
+  }, [productId]);
 
   return (
     <>
