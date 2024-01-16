6d17f8e11cd12a482ea1712533a216faede09eee
Moballigh5225 <moballigh5225@gmail.com>
2024-01-17 03:32:42 +0530

    fetched api data using redux toolkit rtk query

---
+++ frontend/src/slices/productApiSlice.js
@@ -0,0 +1,25 @@
+import { PRODUCTS_URL, DETAIL_URL } from "../constants";
+import { apiSlice } from "./apiSlice";
+
+export const productsApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    // The query accepts a number and returns a Post
+    getProducts: builder.query({
+      // note: an optional `queryFn` may be used in place of `query`
+      // we need object so we are using parenthesis here ({})
+      query: () => ({
+        url: PRODUCTS_URL,
+      }),
+      keepUnusedDataFor: 5,
+    }),
+    getProductDetails: builder.query({
+      query: (productId) => ({
+        url: `${PRODUCTS_URL}/${productId}`,
+      }),
+      keepUnusedDataFor: 5,
+    }),
+  }),
+});
+
+export const { useGetProductsQuery, useGetProductDetailsQuery } =
+  productsApiSlice;
---
+++ frontend/src/store.js
@@ -0,0 +1,19 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { apiSlice } from "./slices/apiSlice";
+
+const store = configureStore({
+  reducer: {
+    // Add the generated reducer as a specific top-level slice
+    [apiSlice.reducerPath]: apiSlice.reducer,
+  },
+  // Adding the api middleware enables caching, invalidation, polling,
+  // and other useful features of `rtk-query`.
+
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
+
+  // we have another option to use devtools also.you could set this to true if you are only in development
+  devTools: true,
+});
+
+export default store;
