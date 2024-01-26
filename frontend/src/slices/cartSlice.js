8f24861c261ad049ddad893b5f30423e0ec4893b
Moballigh5225 <moballigh5225@gmail.com>
2024-01-27 05:24:08 +0530

    addToCart and removeFromCart reducer implemented

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -16,8 +16,8 @@ const cartSlice = createSlice({
     addToCart: (state, action) => {
       // console.log(action.payload._id, "action id here");
       const item = action.payload;
-      console.log(item, "item is here");
-      const existItem = state.cartItems.find((x) => x._id === item.id);
+      // console.log(item, "item is here");
+      const existItem = state.cartItems.find((x) => x._id === item._id);
 
       // here we are comparing the id of item which is already stored in the local storage or statically created initial state
       // if after comparing its get proved that the id of the stored items product is equal to the existed item which is id which we got from the application side then we will print the item information but if it is not equal then we simple print the initial cartItems
@@ -34,10 +34,15 @@ const cartSlice = createSlice({
 
       return updateCart(state);
     },
+    removeFromCart: (state, action) => {
+      state.cartItems = state.cartItems.filter((x) => x._id !== action.payload);
+
+      return updateCart(state);
+    },
   },
 });
 
 // exporting actions
-export const { addToCart } = cartSlice.actions;
+export const { addToCart, removeFromCart } = cartSlice.actions;
 
 export default cartSlice.reducer;
