e14022ca194ce90652c64f0ea8e2b6752c84aa99
Moballigh5225 <moballigh5225@gmail.com>
2024-02-20 09:43:24 +0530

    order screen created

--- backend/server.js
+++ backend/server.js
@@ -24,8 +24,8 @@ app.get("/", (req, res) => {
   res.send("API is running......");
 });
 
-app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/products", productRoutes);
 app.use("/api/orders", orderRoutes);
 // app.use(notFound);
 // app.use(errorHandler);
